f6446858ede2a850d7fc0ae31fe707235ed45b48
Ifera <iferareaves@gmail.com>
2023-08-28 17:24:34 +0500

    throw errors instead

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,21 +1,19 @@
 const Joi = require('joi');
+const { BadRequest } = require('../utils/error');
 const { USE_MONGO_DB } = require('../globals');
 
-// eslint-disable-next-line consistent-return
 module.exports.validateQueryParams = (validator) => (req, res, next) => {
   const { error } = validator(req.query);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) throw new BadRequest(error.details[0].message);
   next();
 };
 
-// eslint-disable-next-line consistent-return
 module.exports.validateReq = (validator) => (req, res, next) => {
   const { error } = validator(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) throw new BadRequest(error.details[0].message);
   next();
 };
 
-// eslint-disable-next-line consistent-return
 module.exports.validateBulkDelete = (req, res, next) => {
   const bulkDeleteSchema = Joi.object({
     ids: Joi.array()
@@ -25,7 +23,7 @@ module.exports.validateBulkDelete = (req, res, next) => {
   });
 
   const { error } = bulkDeleteSchema.validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) throw new BadRequest(error.details[0].message);
 
   next();
 };
